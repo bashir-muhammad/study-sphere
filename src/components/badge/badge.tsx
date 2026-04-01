60ab5d10a5f8f44617912d12514a7002b6aa8a86
karimim <muhammad.karimi@post.ch>
2026-04-01 15:19:30 +0200

    feat: replace the label to children

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,16 +1,25 @@
-import type { HTMLAttributes } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
 import Styles from "./badge.module.css";
 
 interface BadgeProps extends HTMLAttributes<HTMLDivElement> {
-  label: string;
+  children: ReactNode;
   variant?: "primary" | "tertiary" | "success" | "warning" | "error";
 }
 
-const Badge = ({ label, variant = "primary", className }: BadgeProps) => {
+const Badge = ({
+  children,
+  variant = "primary",
+  className,
+  ...props
+}: BadgeProps) => {
   const combinedClassName = [Styles.badge, Styles[variant], className]
     .filter(Boolean)
     .join(" ");
-  return <div className={combinedClassName}>{label}</div>;
+  return (
+    <div className={combinedClassName} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export { Badge };
