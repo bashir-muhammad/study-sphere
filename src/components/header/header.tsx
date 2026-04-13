1c2c9b89f54a86f4750ada3aab270beca28f0f23
karimim <muhammad.karimi@post.ch>
2026-04-13 15:07:17 +0200

    feat: add links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,11 +8,30 @@ const Header = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
 
   return (
     <header className={Styles.header} {...props}>
-      <div className={combinedClasses}>
+      <div className={`${combinedClasses} ${Styles.headerMain}`}>
         <div className={Styles.logo}>
           <Link href={"/"}>{<Logo />}</Link>
           <span className="sr-only">Logo: Dimando questionnaire</span>
         </div>
+        <nav>
+          <ul className={Styles.navList}>
+            <li>
+              <Link href={"/"} className={Styles.navLink}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href={"/learn"} className={Styles.navLink}>
+                Learn
+              </Link>
+            </li>
+            <li>
+              <Link href={"/create"} className={Styles.navLink}>
+                Create deck
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
