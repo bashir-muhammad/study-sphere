df28f183e3a525c5d11f6527f9d3b36343eae475
bashir-muhammad <bashirkarimi5@gmail.com>
2026-06-03 14:27:04 +0200

    chore: refactor the progress overview to new module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 import Styles from "./page.module.css";
 import { useApp } from "@/context/app-context";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/card/card";
-import { Progress, ProgressLabel } from "@/components/progress/progress";
 import { PageTitle, PageDescription } from "@/components/page-title/page-title";
-import { Badge } from "@/components/badge/badge";
 import { DecksList } from "@/app/modules/decks-list/decks-list";
+import { ProgressOverview } from "@/app/modules/progress-overview/progress-overview";
 
 export default function Home() {
   const { state } = useApp();
@@ -24,28 +17,7 @@ export default function Home() {
           maintain your momentum.
         </PageDescription>
       </header>
-      <section className={Styles.cards}>
-        {state.stats.map((stat) => (
-          <Card key={stat.deckId} className={Styles.card}>
-            <CardHeader>
-              <Badge>{stat.score}% mastered</Badge>
-            </CardHeader>
-            <CardTitle>{stat.title}</CardTitle>
-            <CardDescription>
-              <p>
-                {stat.totalCards} {stat.totalCards === 1 ? " Card" : " Cards"}
-              </p>
-              <ProgressLabel>
-                Progress
-                <span className={Styles.labelleft}>
-                  {stat.correctAnswers}/{stat.totalCards}
-                </span>
-              </ProgressLabel>
-              <Progress value={`${stat.score}%`} variant="tertiary" />
-            </CardDescription>
-          </Card>
-        ))}
-      </section>
+      {state.stats.length > 0 && <ProgressOverview stats={state.stats} />}
       {state.decks.length > 0 && <DecksList decks={state.decks} />}
     </div>
   );
