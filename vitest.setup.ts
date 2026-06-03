c8edcd9a28db02976bd6df53ea57704bf2acf8a6
bashir-muhammad <bashirkarimi5@gmail.com>
2026-06-03 11:09:59 +0200

    feat: add Test for DecksList and support for SVGs in Vitest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,16 @@
 import "@testing-library/jest-dom/vitest";
+import { forwardRef, createElement, SVGProps } from "react";
+
+const createSVGMock = () => ({
+  default: forwardRef<SVGElement, SVGProps<SVGElement>>(
+    function SVGMock(props) {
+      return createElement("svg", { ...props });
+    },
+  ),
+});
+
+vi.mock("@/assets/icons/arrow-right.svg", () => createSVGMock());
+vi.mock("@/assets/icons/arrow-left.svg", () => createSVGMock());
+vi.mock("@/assets/icons/double-arrow-right.svg", () => createSVGMock());
+vi.mock("@/assets/icons/list-alt-check.svg", () => createSVGMock());
+vi.mock("@/assets/icons/logo.svg", () => createSVGMock());
